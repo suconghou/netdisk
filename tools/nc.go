c30ba242a54568d812973bd010256e225a866a48
suconghou <1126045770@qq.com>
2018-03-12 01:30:31 +0800

    up

--- tools/nc.go
+++ tools/nc.go
@@ -21,12 +21,12 @@ type tcpCounter struct {
 }
 
 func progress(totalr uint64, readed uint64, totalw uint64, writed uint64, duration float64) {
-	sends := float64(writed) / 1024 / duration
-	receives := float64(readed) / 1024 / duration
 	var (
-		s1 string
-		s2 string
+		s1, s2   string
+		sends    = float64(writed) / 1024 / duration
+		receives = float64(readed) / 1024 / duration
 	)
+	fmt.Println(sends, receives)
 	if totalw > 0 {
 		s1 = fmt.Sprintf("/%s/%d%%", utilgo.ByteFormat(totalw), 100*writed/totalw)
 	}
@@ -37,23 +37,27 @@ func progress(totalr uint64, readed uint64, totalw uint64, writed uint64, durati
 }
 
 func (c *tcpCounter) Read(p []byte) (int, error) {
-	n, err := c.TCPConn.Read(p)
-	if err != nil {
-		return n, err
-	}
-	c.readed += uint64(n)
-	progress(c.totalr, c.readed, c.totalw, c.writed, time.Since(c.startTime).Seconds())
-	return n, err
+	// n, err := c.TCPConn.Read(p)
+	// fmt.Println("read", n, err)
+	// if err != nil {
+	// 	return n, err
+	// }
+	// c.readed += uint64(n)
+	// progress(c.totalr, c.readed, c.totalw, c.writed, time.Since(c.startTime).Seconds())
+	// return n, err
+	return 0, nil
 }
 
 func (c *tcpCounter) Write(p []byte) (int, error) {
-	n, err := c.TCPConn.Write(p)
-	if err != nil {
-		return n, err
-	}
-	c.writed += uint64(n)
-	progress(c.totalr, c.readed, c.totalw, c.writed, time.Since(c.startTime).Seconds())
-	return n, err
+	// n, err := c.TCPConn.Write(p)
+	// fmt.Println("write", n, err)
+	// if err != nil {
+	// 	return n, err
+	// }
+	// c.writed += uint64(n)
+	// progress(c.totalr, c.readed, c.totalw, c.writed, time.Since(c.startTime).Seconds())
+	// return n, err
+	return 0, nil
 }
 
 // NcMain start
@@ -101,6 +105,7 @@ func ncClient(addr string, progress bool) error {
 	wg.Add(2)
 	if progress {
 		c := &tcpCounter{TCPConn: conn.(*net.TCPConn), startTime: time.Now(), totalw: uint64(123455)}
+		fmt.Println(c, "progress")
 		go func() { io.Copy(c, os.Stdin); c.CloseWrite(); wg.Done() }()
 		go func() { io.Copy(os.Stdout, c); c.CloseRead(); wg.Done() }()
 	} else {
